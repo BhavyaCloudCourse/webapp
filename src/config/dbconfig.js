9e9256339980b16edc042e841072e1d6592728d4
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-07 17:02:44 -0500

    Uploading assignment01 and created readme and job

---
+++ src/config/dbconfig.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+});
+
+const authenticate = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    throw error;
+  }
+};
+
+module.exports = { sequelize, authenticate };
