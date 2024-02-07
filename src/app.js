ff3158f97fc356d49af9bb8f9a3711361f6ab31a
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-07 17:25:19 -0500

    Added database sync function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 const express = require('express');
-const healthzRouter = require('./routes/healthz');
+const healthzRouter = require('./routes/healthzRoutes');
+const userRouter = require('./routes/userRoutes');
+const sequelize = require('./config/dbconfig');
+const user  = require('./models/user');
 require('dotenv').config();
-
-
-
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use(express.json());
 
-// Health check route
-app.use('/healthz',healthzRouter);
+(async () => {
+  try {
+    // Authenticate with the database
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    // Sync the User model with the database
+    await sequelize.sync({ alter: true });
+    // Middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    // Routes
+    app.use('/healthz',healthzRouter);
+    app.use('/v1/user', userRouter);
+    
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
