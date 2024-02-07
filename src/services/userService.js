9afe8607e0032d11ac07f1134cac81b688cedb8c
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-07 17:41:45 -0500

    Created user services

---
+++ src/services/userService.js
@@ -0,0 +1,42 @@
+
+const User = require('../models/user');
+
+const createUser = async (userData) => {
+  try {
+    const user = await User.create(userData);
+
+    return user;
+  } catch (error) {
+    console.log(error)
+    throw error;
+  }
+};
+const getUserByUsername = async (username) => {
+    try {
+      const user = await User.findOne({
+        where: { username }
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const updateUser = async (userId, updatedData) => {
+    try {
+      const user = await User.findByPk(userId);
+      if (user) {
+        user.first_name =updatedData.first_name;
+        user.last_name= updatedData.last_name;
+        user.password=updatedData.password;
+        user.account_updated= new Date();
+        await user.save();
+        return user;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+module.exports = { createUser,getUserByUsername, updateUser};
