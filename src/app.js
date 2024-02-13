3a571d7b2ec307dedbd9b7f3bae01b0fc8af7848
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-13 00:01:45 -0500

    Created integration tests

--- src/app.js
+++ src/app.js
@@ -5,48 +5,59 @@ const sequelize = require('./config/dbconfig');
 const user  = require('./models/user');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 // Define middleware function to set response headers
-const setResponseHeaders = (req, res, next) => {
-  // Set response headers
-  res.set({
-    'access-control-allow-credentials': 'true',
-    'access-control-allow-headers': 'X-Requested-With,Content-Type,Accept,Origin',
-    'access-control-allow-methods': '*',
-    'access-control-allow-origin': '*',
-    'cache-control': 'no-cache',
-    'content-type': 'application/json;charset=utf-8',
-    'etag': 'W/"a9-+UP6xBjsYc82q+jeDmgihwDu6zk"',
-    'expires': '-1',
-    'server': 'nginx',
-  });
+// const setResponseHeaders = (req, res, next) => {
+//   // Set response headers
+//   res.set({
+//     'access-control-allow-credentials': 'true',
+//     'access-control-allow-headers': 'X-Requested-With,Content-Type,Accept,Origin',
+//     'access-control-allow-methods': '*',
+//     'access-control-allow-origin': '*',
+//     'cache-control': 'no-cache',
+//     'content-type': 'application/json;charset=utf-8',
+//     'etag': 'W/"a9-+UP6xBjsYc82q+jeDmgihwDu6zk"',
+//     'expires': '-1',
+//     'server': 'nginx',
+//   });
 
-  // Move to the next middleware or route handler
-  next();
-};
+//   // Move to the next middleware or route handler
+//   next();
+// };
 
-(async () => {
-  try {
-    // Authenticate with the database
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully.');
+// (async () => {
+//   try {
+//     // Authenticate with the database
+//     await sequelize.authenticate();
+//     console.log('Database connection has been established successfully.');
 
-    // Sync the User model with the database
-    await sequelize.sync({ alter: true });
-    // Middleware
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(setResponseHeaders);
-    // Routes
-    app.use('/healthz',healthzRouter);
-    app.use('/v1/user', userRouter);
+//     // Sync the User model with the database
+//     await sequelize.sync({ alter: true });
+//     // Middleware
+//     app.use(express.json());
+//     app.use(express.urlencoded({ extended: false }));
+//     app.use(setResponseHeaders);
+//     // Routes
+//     app.use('/healthz',healthzRouter);
+//     app.use('/v1/user', userRouter);
     
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// })();
 
-    // Start the server
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+async function startApp() {
+  try {
+      await sequelize.sync({ alter: true });
+      console.log('Database synced successfully');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+      console.error('Error syncing database:', error);
   }
-})();
+}
+startApp();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/healthz',healthzRouter);
+app.use('/v1/user', userRouter);
+
+module.exports=app;
