ab07b16ddd0afcec4cab5485c01002cc7944f621
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-21 15:46:51 -0500

    added packer image name timestamp

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ sudo echo "The current username is: $USER"
 ls /home/admin
 cd /home/admin || exit
 sudo groupadd csye6225
-sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225  -m csye6225
+sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225  -m csye6225
 sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
 cd /opt/csye6225 || exit
 sudo unzip -o webapp.zip
@@ -51,8 +51,8 @@ sudo -rf node_modules
 sudo rm package-lock.json
 sudo npm install
 sudo echo "The current username is: $USER"
-sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/webapp
-sudo chmod -R 750 /opt/csye6225/webapp/webapp
+# sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/webapp
+# sudo chmod -R 750 /opt/csye6225/webapp/webapp
 sudo chmod -R 700 /var/lib/mysql
 sudo cat << EOF | sudo tee /etc/systemd/system/csye6225.service
 [Unit]
