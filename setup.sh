6faa81772f4036e76e87dd3a9da82f16913f4414
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-28 21:46:28 -0500

    removed local db

--- setup.sh
+++ setup.sh
@@ -4,27 +4,11 @@
 sudo dnf update -y
 
 
-# Install MySQL 8
-sudo dnf install mysql-server -y
-sudo systemctl start mysqld
-sudo systemctl enable mysqld
-
-
-#Create database
-sudo mysql -u root -e "create database cloud"
-sudo mysql -u root -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASSWD'"
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON cloud.* TO '$DBUSER'@'localhost';"
-sudo mysql -u root -e "FLUSH PRIVILEGES;"
-sudo mysql -u root -e "SHOW DATABASES";
-
-
 # Install Node.js 20 using NodeSource repository
 curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
 sudo dnf install -y nodejs
 
 # Verify installations
-echo "MySQL version:"
-mysql --version
 echo "Node.js version:"
 node --version
 echo "npm version:"
@@ -51,8 +35,6 @@ sudo -rf node_modules
 sudo rm package-lock.json
 sudo npm install
 sudo echo "The current username is: $USER"
-# sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/webapp
-# sudo chmod -R 750 /opt/csye6225/webapp/webapp
 sudo chmod -R 700 /var/lib/mysql
 sudo cat << EOF | sudo tee /etc/systemd/system/csye6225.service
 [Unit]
@@ -81,8 +63,5 @@ sudo systemctl daemon-reload
 sudo systemctl enable csye6225.service
 sudo systemctl start csye6225.service
 
-sudo journalctl -u csye6225.service
-sudo netstat -ln | grep 3000
-sudo netstat -tulpn
 
 
