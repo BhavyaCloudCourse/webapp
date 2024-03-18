1847a67e939d77af85152266137bb227632d8615
Bhavya Prakash <prakash.b@northeastern.edu>
2024-03-18 03:24:34 -0400

    created log file

--- src/app.js
+++ src/app.js
@@ -4,56 +4,32 @@ const userRouter = require('./routes/userRoutes');
 const sequelize = require('./config/dbconfig');
 const user  = require('./models/user');
 require('dotenv').config();
+const winston = require('winston');
 const app = express();
-// const PORT = process.env.PORT || 8080;
-// Define middleware function to set response headers
-// const setResponseHeaders = (req, res, next) => {
-//   // Set response headers
-//   res.set({
-//     'access-control-allow-credentials': 'true',
-//     'access-control-allow-headers': 'X-Requested-With,Content-Type,Accept,Origin',
-//     'access-control-allow-methods': '*',
-//     'access-control-allow-origin': '*',
-//     'cache-control': 'no-cache',
-//     'content-type': 'application/json;charset=utf-8',
-//     'etag': 'W/"a9-+UP6xBjsYc82q+jeDmgihwDu6zk"',
-//     'expires': '-1',
-//     'server': 'nginx',
-//   });
-
-//   // Move to the next middleware or route handler
-//   next();
-// };
-
-// (async () => {
-//   try {
-//     // Authenticate with the database
-//     await sequelize.authenticate();
-//     console.log('Database connection has been established successfully.');
-
-//     // Sync the User model with the database
-//     await sequelize.sync({ alter: true });
-//     // Middleware
-//     app.use(express.json());
-//     app.use(express.urlencoded({ extended: false }));
-//     app.use(setResponseHeaders);
-//     // Routes
-//     app.use('/healthz',healthzRouter);
-//     app.use('/v1/user', userRouter);
-    
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// })();
-
 async function startApp() {
   try {
       await sequelize.sync({ alter: true });
+      logger.info('Database synced successfully');
       console.log('Database synced successfully');
   } catch (error) {
+      logger.error('Error syncing database:', error);
       console.error('Error syncing database:', error);
   }
 }
+
+// Create a Winston logger instance
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(), // Include timestamp in log message
+    winston.format.json() // Use JSON format for logs
+  ),
+  transports: [
+    // Add a file transport to write logs to a file
+    new winston.transports.File({ 
+      filename: '/var/log/webapp.log', // Adjust file path as needed
+    })
+  ],
+});
 startApp();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
