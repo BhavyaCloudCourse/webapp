f2992be894cbb03cf02a83c44811ac425bb7232a
Bhavya Prakash <prakash.b@northeastern.edu>
2024-03-26 01:54:05 -0400

    Added logs and pubsub env var

--- src/controllers/healthzController.js
+++ src/controllers/healthzController.js
@@ -1,4 +1,5 @@
 const sequelize = require('../config/dbconfig');
+const logger = require('../utils/logger');
 
 const apiHealthCheck = async (req, res) => {
     try {
@@ -6,26 +7,30 @@ const apiHealthCheck = async (req, res) => {
           console.log(req.method);
           // Verify that only HTTP GET method is supported
           if (req.method !== 'GET') {
-            console.log("1");
+            logger.warn('Incorrect method type used');
+            logger.error('Incorrect method type used');
+            logger.debug('Use Get method type');
             return res.status(405).set({ 'Cache-Control': 'no-cache, no-store, must-revalidate', 'Pragma': 'no-cache', 'X-Content-Type-Options': 'nosniff' }).send();
           }
           console.log(Object.keys(req.body).length,Object.keys(req.query).length);
           // Verify no body and params and in the request
           if((Object.keys(req.body).length>0) || (Object.keys(req.query).length>0)) {
-            console.log("2");
+            logger.warn('No request body or params required');
+            logger.error('No request body or params required');
+            logger.debug('Send request without body and params');
             return res.status(400).set({ 'Cache-Control': 'no-cache, no-store, must-revalidate', 'Pragma': 'no-cache', 'X-Content-Type-Options': 'nosniff' }).send();
           }
-          console.log("3");
           // Authenticate with the database
           await sequelize.authenticate();
-          console.log("4");
           // Return HTTP 200 OK if successful
           res.status(200).set({ 'Cache-Control': 'no-cache, no-store, must-revalidate', 'Pragma': 'no-cache', 'X-Content-Type-Options': 'nosniff' }).send();
+          logger.info('Database succssfully authenticated');
   } 
   catch (error) {
       // Return HTTP 503 Service Unavailable if unsuccessful
-      console.log("5");
-      console.log(error);
+      logger.warn('Error in authenticating database');
+      logger.error('Error in authenticating database',error);
+      logger.debug("Check database instance is running");
       res.status(503).set({ 'Cache-Control': 'no-cache, no-store, must-revalidate', 'Pragma': 'no-cache', 'X-Content-Type-Options': 'nosniff' }).send();
   }
   };
